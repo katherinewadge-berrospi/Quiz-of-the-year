8469a1b6f351db1554e2d4550c0e02775e8d523f
katherine wadge-berrospi <katherinewadge@gmail.com>
2024-12-06 10:39:24 +0000

    Added getters to js

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+let question = document.getElementById("question");
+let answerOne = document.getElementById("answerOne");
+let answerTwo = document.getElementById("answerTwo");
+let answerThree = document.getElementById("answerThree");
+let answerFour = document.getElementById("answerFour");
+
+let score = document.getElementById("score");
+let playAgain = document.getElementById("playAgain");
