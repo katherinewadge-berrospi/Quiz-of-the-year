bf74858de9fce6572ebcaa241ca9a3635e2f2154
katherine wadge-berrospi <katherinewadge@gmail.com>
2024-12-06 10:48:16 +0000

    Added background structure to questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,35 @@
-let question = document.getElementById("question");
-let answerOne = document.getElementById("answerOne");
-let answerTwo = document.getElementById("answerTwo");
-let answerThree = document.getElementById("answerThree");
-let answerFour = document.getElementById("answerFour");
+// getElementById...
+const question = document.getElementById("question");
+const answerOne = document.getElementById("answerOne");
+const answerTwo = document.getElementById("answerTwo");
+const answerThree = document.getElementById("answerThree");
+const answerFour = document.getElementById("answerFour");
 
-let score = document.getElementById("score");
-let playAgain = document.getElementById("playAgain");
+const score = document.getElementById("score");
+const playAgain = document.getElementById("playAgain");
+
+let questionNum = 0;
+let scoreNum = 0;
+
+const questions = [
+    {
+        "question": What is question 1?,
+        "answers": ["1a", "1b", "1c", "1d"],
+        "correct": 2
+    },
+    {
+        "question": What is question 2?,
+        "answers": ["2a", "2b", "3c", "4d"],
+        "correct": 0
+    },
+    {
+        "question": What is question 3?,
+        "answers": ["3a", "3b", "3c", "3d"],
+        "correct": 3
+    },
+    {
+        "question": What is question 4?,
+        "answers": ["4a", "4b", "4c", "4d"],
+        "correct": 1
+    },
+]
